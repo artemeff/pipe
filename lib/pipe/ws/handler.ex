3dcdec888c96c61d29fafdc3ca8477e7cd708c53
Yuri Artemev <i@artemeff.com>
2013-08-17 17:56:58 +0400

    first implementation of ws

--- lib/pipe/ws/handler.ex
+++ lib/pipe/ws/handler.ex
@@ -1,42 +1,98 @@
 defmodule Pipe.Ws.Handler do
+  @moduledoc """
+  
+  State is:
 
-  @period 1000
+    `[:active, :user_session, :user_id]`
 
-  def init(_transport, request, _options, _active) do
-    IO.puts " -- [ws]   ws init"
+  Where `:active` is
 
-    { :ok, request, :test }
+    * `true`  - WebSocket connection
+    * `:once` - Polling connection
+
+  """
+
+  ##
+  # Connect to ws server
+  #
+  # :ok       - all fine, just work
+  # :shutdown - close connection in websockets and
+  #   try to reconnect in polling
+  ##
+
+  def init(_transport, request, _options, active) do
+    # define state
+    state = { active, nil, nil }
+
+    # get token
+    { token, _ } = :cowboy_req.cookie("_token", request)
+
+    # auth user
+    case :gen_server.call(:users, { :session, token }) do
+      user_id when is_integer(user_id) ->
+        # set new state
+        new_state = state |> set_elem(1, token) |> set_elem(2, user_id)
+
+        # subscribe user
+        :gproc.reg({ :p, :l, user_id })
+
+        # send ok new_state
+        # { :ok, request, new_state }
+
+      false ->
+        # falsy
+        # { :shutdown, request, :unathorized }
+        false
+
+    end
+
+    { :ok, request, state }
+    #{ :shutdown, request, :ok }
   end
 
-  
-  #def stream(data, request, state) do
-  #  IO.puts " -- [ws]   stream ok: '#{data}'"
+  ##
+  # Receive -> send message, it method touch by user data,
+  # eg when anybody sends message to our ws/polling server
+  # it method runs
   #
-  #  { :ok, request, state }
-  #end
+  # :ok    - just say to client that request is delivered
+  # :reply - request is delivered and sand message with response
+  ##
 
   def stream(data, request, state) do
-    IO.puts " -- [ws]   stream re: '#{data}'"
+    # get data from state
+    { active, user_session, user_id } = state
 
-    { :reply, "stream: #{data};#{state}", request, state }
+    :gproc.send({ :p, :l, user_id }, "sess: #{user_session}")
+
+    #{ :ok, request, state }
+    { :reply, "notify: #{rand}", request, state }
   end
 
-
-  #def info(message, request, state) do 
-  #  IO.puts " -- [ws]   info ok: '#{message}'"
+  ##
+  # Send message by server, it method should touch by server
+  # and its send any data to client
   #
-  #  { :ok, request, state }
-  #end
+  # :ok    - just say to client that request is delivered
+  # :reply - request is delivered and sand message with response
+  ##
 
   def info(message, request, state) do 
-    IO.puts " -- [ws]   info re: '#{message}'"
+    #IO.write " -- [ws]   info re: '#{message}'"
 
-    { :reply, "message: #{message};#{state}", request, state }
+    #{ :ok, request, state }
+    { :reply, "message: #{message}", request, state }
   end
 
+  ##
+  # Shutdown connect
+  #
+  # :ok
+  ##
 
   def terminate(_request, _state) do
-    IO.puts " -- [ws]   terminate"
+    #IO.write " -- [ws]   terminate with state: "
+    #IO.inspect state
 
     :ok
   end
