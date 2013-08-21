6e2ca88edf92e1cefc6db5324652507cc72071f5
Yuri Artemev <i@artemeff.com>
2013-08-22 03:47:56 +0400

    add simple http auth and primitive api

--- lib/pipe/api/handler.ex
+++ lib/pipe/api/handler.ex
@@ -1,10 +1,24 @@
 defmodule Pipe.Api.Handler do
 
-  def init(_transport, req, []), do:
-    { :ok, req, :undefined }
+  # TODO
+  # https://github.com/extend/cowboy/blob/master/examples/echo_post/src/toppage_handler.erl
 
-  def handle(req, state) do
+  def init(_transport, req, []) do
+    { key, new_req } = :cowboy_req.header("x-th-auth", req, false)
+
+    case key do
+      "1" -> { :ok, new_req, true }
+       _  -> { :ok, new_req, false }
+    end
+  end
+
+  def handle(req, state) when state do
     { :ok, new_req } = :cowboy_req.reply(200, [], "Hello world!", req)
+    { :ok, new_req, state}
+  end
+
+  def handle(req, state) when not state do
+    { :ok, new_req } = :cowboy_req.reply(401, [], "Unauthorized", req)
     { :ok, new_req, state}
   end
 
